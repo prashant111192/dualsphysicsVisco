a13f038c51553bb946101eefac60a35d723a910d
Prashant Kumar <prashant_1111@hotmail.com>
2020-02-07 14:10:58 +0100

    NothingAdded

--- source/JSphCpu.cpp
+++ source/JSphCpu.cpp
@@ -920,8 +920,8 @@ template<bool psingle,TpKernel tker,TpFtMode ftmode,bool lamsps,TpDeltaSph tdelt
   //-Initialise execution with OpenMP. | Inicia ejecucion con OpenMP.
   const int pfin=int(pinit+n);
   #ifdef OMP_USE
-    #pragma omp parallel for schedule (guided)
-  #endif
+  #pragma omp parallel for schedule (guided)
+ #endif
   for(int p1=int(pinit);p1<pfin;p1++){
     float visc=0,arp1=0,deltap1=0;
     tfloat3 acep1=TFloat3(0);
@@ -1040,6 +1040,7 @@ template<bool psingle,TpKernel tker,TpFtMode ftmode,bool lamsps,TpDeltaSph tdelt
                   acep1.x-=pi_visc*frx; acep1.y-=pi_visc*fry; acep1.z-=pi_visc*frz;
                 }
               }
+              //https://www.sciencedirect.com/science/article/pii/S002199911200229X#b0015
 */
             //===== Artificial Viscosity =====AND PRESSURE COMBINED
             if(compute){
@@ -1097,15 +1098,15 @@ template<bool psingle,TpKernel tker,TpFtMode ftmode,bool lamsps,TpDeltaSph tdelt
 
               if(compute && !boundp2)
               {
-                  const float NU1 = 0.801;
+                  //const float NU1 = 0.801;
 
-                  //const float NU1 = 0.798;
+                  const float NU1 = 0.798;
 
                  const float va = (massp1/rhopp1); //Volume
                  const float vb = (massp2/velrhop2.w);
                   const float robar=(rhopp1+velrhop2.w);
-                  //const float mu_ij_Vol = 1 * NU1 * (va*va+ vb*vb)/massp1;
-                  const float mu_ij_Vol = 2 * NU1 * ((rhopp1*velrhop2.w)/robar)*(va*va+ vb*vb)/massp1;
+                  const float mu_ij_Vol = 1 * NU1 * (va*va+ vb*vb)/massp1;
+                 // const float mu_ij_Vol = 2 * NU1 * ((rhopp1*velrhop2.w)/robar)*(va*va+ vb*vb)/massp1;
                   /*if (mu_ij_Vol>340282346638528859811704183484516925440.0000000000000000  || mu_ij_Vol <.000000000000000000000000000000000000000000000000000000000000000000000000000000000000001)
                   {
                       const float mu_ij_Vol = 2 * NU1 * ((rhopp1*velrhop2.w)/robar)*(va*va+ vb*vb);
