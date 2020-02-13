0c84f29bf7237cd85e7dd1166d739150d782cbe6
Prashant Kumar <prashant_1111@hotmail.com>
2020-02-13 17:03:21 +0100

    It works ?? ;-P

--- source/JSphCpu.cpp
+++ source/JSphCpu.cpp
@@ -920,7 +920,7 @@ template<bool psingle,TpKernel tker,TpFtMode ftmode,bool lamsps,TpDeltaSph tdelt
   //-Initialise execution with OpenMP. | Inicia ejecucion con OpenMP.
   const int pfin=int(pinit+n);
   #ifdef OMP_USE
-  //#pragma omp parallel for schedule (guided)
+    #pragma omp parallel for schedule (guided)
   #endif
   for(int p1=int(pinit);p1<pfin;p1++){
     float visc=0,arp1=0,deltap1=0;
@@ -1116,13 +1116,15 @@ template<bool psingle,TpKernel tker,TpFtMode ftmode,bool lamsps,TpDeltaSph tdelt
               //if(compute){
                   //const float NU1 = 5.801;
                   //const float NU1 = 0.801;
-                  const float NU1 = 0.798;
+                  //const float NU1 = 0.798;
+                  const float mass11 = .1;
+                  const float NU1 = 934.0;
                   const float va = (massp1/rhopp1); //Volume
                   const float vb = (massp2/velrhop2.w);
                   //const float robar=(rhopp1+velrhop2.w);
                   //const float mu_ij_Vol = 1 * NU1 * (va*va+ vb*vb)/massp1;
                   //const float mu_ij_Vol = 2 * NU1 * ((rhopp1*velrhop2.w)/robar)*(va*va+ vb*vb)/massp1;
-                  const float mu_ij_Vol = 1 * NU1 * (va*va+ vb*vb)/1;
+                  const float mu_ij_Vol = 1 * NU1 * (va*va+ vb*vb);
                   //if (mu_ij_Vol>340282346638528859811704183484516925440.0000000000000000  || mu_ij_Vol <.000000000000000000000000000000000000000000000000000000000000000000000000000000000000001)
                   //{
                       //const float mu_ij_Vol = 2 * NU1 * ((rhopp1*velrhop2.w)/robar)*(va*va+ vb*vb);
@@ -1142,7 +1144,9 @@ template<bool psingle,TpKernel tker,TpFtMode ftmode,bool lamsps,TpDeltaSph tdelt
 
 
 
-                  acep1.x+=visc_factor_x*fac; acep1.y+=visc_factor_y*fac; acep1.z+=visc_factor_z*fac;
+                  //acep1.x+=visc_factor_x*fac; acep1.y+=visc_factor_y*fac; acep1.z+=visc_factor_z*fac;
+                  acep1.x+=visc_factor_x*frx; acep1.y+=visc_factor_y*fry; acep1.z+=visc_factor_z*frz;
+
                   int zz =acep1.x;
                   zz++;
               }
